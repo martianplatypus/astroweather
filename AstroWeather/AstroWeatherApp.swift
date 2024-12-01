37bc5739821a88188678d867de3cd8dad80366ee
Luiz Rath <44349218+martianplatypus@users.noreply.github.com>
2024-12-01 13:32:10 -0300

    AstroWeather skeleton project running on iOS 15+

---
+++ AstroWeather/AstroWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroWeatherApp.swift
+//  AstroWeather
+//
+//  Created by Luiz Rath on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct AstroWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
