ad44ecc52ec0c4ef48d18a21755341d4c9f22ae5
Luiz Rath <44349218+martianplatypus@users.noreply.github.com>
2024-12-03 13:09:46 -0300

    Improving logger functionality

--- Utils/Logger.swift
+++ Utils/Logger.swift
@@ -8,13 +8,20 @@
 import Foundation
 
 protocol Logging {
-    func log(_ string: String)
+    static func log(_ message: String, _ function: String, _ file: String, _ line: Int)
 }
 
-final class Logger: Logging {
-    func log(_ string: String) {
+extension Logging {
+    static func log(_ message: String, _ function: String = #function, _ file: String = #file, _ line: Int = #line) {
+        Self.log(message, function, file, line)
+    }
+}
+
+struct Logger: Logging {
+    static func log(_ message: String, _ function: String, _ file: String, _ line: Int) {
+        let fileName = URL(fileURLWithPath: file).lastPathComponent
 #if DEBUG
-        print(string)
+        print("[\(fileName):\(line)] \(function) - \(message)")
 #endif
     }
 }
